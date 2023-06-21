9f26cdd049b151f5ead69d6824aecf12aa234cc6
Joshua Shew <joshua.t.shew@gmail.com>
2023-06-21 09:39:49 -0400

    Update test_strtools.py Parameterized tests with pytest

--- tests/unit/test_strtools.py
+++ tests/unit/test_strtools.py
@@ -1,5 +1,6 @@
 """Tests for strtools.py"""
 
+import pytest
 import hypothesis as hp
 import hypothesis.strategies as st
 import zolltools.strtools as strtools
@@ -19,20 +20,32 @@ def test_removeprefix_empty_prefix(string: str):
     assert strtools.removeprefix(string, "") == string
 
 
-def test_removeprefix_with_matching_prefix():
+@pytest.mark.parametrize(
+    ("input", "prefix", "expected"),
+    [
+        ("abcabcabc", "abc", "abcabc"),
+        ("abc123456", "abc", "123456"),
+        ("abc", "a", "bc"),
+    ],
+)
+def test_removeprefix_with_matching_prefix(input, prefix, expected):
     """removeprefix test with a prefix that matches"""
 
-    assert strtools.removeprefix("abcabcabc", "abc") == "abcabc"
-    assert strtools.removeprefix("abc123456", "abc") == "123456"
-    assert strtools.removeprefix("abc", "a") == "bc"
+    assert strtools.removeprefix(input, prefix) == expected
 
 
-def test_removeprefix_with_nonmatching_prefix():
+@pytest.mark.parametrize(
+    ("input", "prefix"),
+    [
+        ("abcabcabc", "bac"),
+        ("abc123456", "123"),
+        ("abc123456", "456"),
+    ],
+)
+def test_removeprefix_with_nonmatching_prefix(input, prefix):
     """removeprefix test with a prefix that does not match"""
 
-    assert strtools.removeprefix("abcabcabc", "bac") == "abcabcabc"
-    assert strtools.removeprefix("abc123456", "123") == "abc123456"
-    assert strtools.removeprefix("abc123456", "456") == "abc123456"
+    assert strtools.removeprefix(input, prefix) == input
 
 
 @hp.given(suffix=st.text())
@@ -48,21 +61,34 @@ def test_removesuffix_empty_suffix(string: str):
 
     assert strtools.removesuffix(string, "") == string
 
-
-def test_removesuffix_with_matching_suffix():
+@pytest.mark.parametrize(
+    ("input", "suffix", "expected"),
+    [
+        ("abcabcabc", "abc", "abcabc"),
+        ("abc123456", "456", "abc123"),
+        ("abc", "c", "ab"),
+    ]
+)
+def test_removesuffix_with_matching_suffix(input, suffix, expected):
     """removesuffix test with a suffix that matches"""
 
-    assert strtools.removesuffix("abcabcabc", "abc") == "abcabc"
-    assert strtools.removesuffix("abc123456", "456") == "abc123"
-    assert strtools.removesuffix("abc", "c") == "ab"
+    assert strtools.removesuffix(input, suffix) == expected
 
 
-def test_removesuffix_with_nonmatching_suffix():
+@pytest.mark.parametrize(
+    ("input", "suffix"),
+    [
+        ("abcabcabc", "bac"),
+        ("abc123456", "123"),
+        ("abc123456", "abc"),
+    ]
+)
+def test_removesuffix_with_nonmatching_suffix(input, suffix):
     """removesuffix test with a suffix that does not match"""
 
-    assert strtools.removesuffix("abcabcabc", "bac") == "abcabcabc"
-    assert strtools.removesuffix("abc123456", "123") == "abc123456"
-    assert strtools.removesuffix("abc123456", "abc") == "abc123456"
+    assert strtools.removesuffix(input, suffix) == input
+    assert strtools.removesuffix(input, suffix) == input
+    assert strtools.removesuffix(input, suffix) == input
 
 
 def main():
