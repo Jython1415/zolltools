0f38cb2974c3fc1a54bfb4e23520590c8cb6ce05
Joshua Shew <joshua.t.shew@gmail.com>
2023-07-11 20:43:17 -0400

    Created logging module Added function header and docstring for add_handler

---
+++ src/zolltools/logging.py
@@ -0,0 +1,23 @@
+"""Convenience module for configuring logging for the zolltools package"""
+
+import logging
+from typing import Union
+
+
+def add_handler(
+    handler: logging.Handler,  # pylint: disable=unused-argument
+    logger: Union[None, str] = None,  # pylint: disable=unused-argument
+    clear=False,  # pylint: disable=unused-argument
+) -> list[logging.Logger]:
+    """
+    Adds `handler` to a logger (or all loggers) in the zolltools package.
+
+    :param handler: the handler to add to the logger(s).
+    :param logger: leave as `None` to apply the handler to all zolltools
+    loggers. Set to a logger name (e.g. "zolltools.nemsis.locationcodes") to
+    select that particular logger.
+    :param clear: set to `True` to clear all other handlers from the logger(s)
+    before adding `handler`.
+    :returns: a list of the loggers the handler was applied to.
+    :raises ValueError: if the logger specified by `logger` cannot be found.
+    """
