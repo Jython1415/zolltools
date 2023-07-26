7bc3538b9beb776bbf131df567819acbec3442bf
Joshua Shew <joshua.t.shew@gmail.com>
2023-07-26 16:10:13 -0400

    Removed main and added type hints to test_sasconvert

--- tests/unit/test_sasconvert.py
+++ tests/unit/test_sasconvert.py
@@ -7,7 +7,7 @@ from zolltools.db.sasconvert import Converter
 
 
 @hp.given(parquet_file_name=st.text())
-def test_get_sas_path_with_name(parquet_file_name: str):
+def test_get_sas_path_with_name(parquet_file_name: str) -> None:
     """_get_sas_path test with a file name"""
 
     if "/" not in parquet_file_name:
@@ -20,7 +20,7 @@ def test_get_sas_path_with_name(parquet_file_name: str):
 
 
 @hp.given(sas_file_name=st.text())
-def test_get_parquet_path_with_name(sas_file_name: str):
+def test_get_parquet_path_with_name(sas_file_name: str) -> None:
     """_get_parquet_path test with a file name"""
 
     if "/" not in sas_file_name:
@@ -30,11 +30,3 @@ def test_get_parquet_path_with_name(sas_file_name: str):
             ).name
             == f"{sas_file_name}.parquet"
         )
-
-
-def main():
-    """Does nothing"""
-
-
-if __name__ == "__main__":
-    main()
